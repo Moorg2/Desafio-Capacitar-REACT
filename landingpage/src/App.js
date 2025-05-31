93ee732e48732b5936e05f5aff39c0a895b5e266
João-Vinicius <Joao60487@gmail.com>
2025-05-31 17:51:39 -0300

    .

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import TopBar from './componentes/Top-bar';
 import Menu from './componentes/Menu'
-import Banner from './componentes/imagens/banner.png'
 import LowBar from './componentes/Low-bar'
 import Produto from './componentes/produtos';
 import './App.css';
@@ -11,32 +10,6 @@ import CadernoImg from './componentes/imagens/Cadernos.png';
 import Logo from './componentes/imagens/logo.png';
 
 export default function App() {
-  const Descricao1 = 'A Papel & Arte oferece serviços completos de personalização de materiais de escritório e papelaria, atendendo a empresas e eventos que buscam destacar sua identidade visual e agregar valor aos seus produtos. Nosso processo começa com a consultoria individualizada, onde entendemos as necessidades específicas de cada cliente, desde pequenas empresas até grandes corporações, adaptando nossos produtos ao perfil de cada marca.'
-  const Descricao2='Além da personalização estética, nos preocupamos com a funcionalidade dos produtos, criando designs que facilitam a organização e a produtividade, sem abrir mão do estilo. Para empresas que desejam presentear funcionários, parceiros ou clientes, disponibilizamos pacotes exclusivos com kits personalizados, que podem incluir itens como agendas, canetas e blocos, tudo em uma embalagem sofisticada e personalizada.'
-  const produtos = [
-    {
-        id: 1,
-        nome: 'Caneta Gel',
-        preco: 'R$ 9,99',
-        descricao: 'Caneta que desliza como manteiga e escreve como um sonho!',
-        imagem: CanetaImg
-    },
-    {
-        id: 2,
-        nome: 'Adesivos Divertidos',
-        preco: 'R$ 14,90',
-        descricao: 'Adesivos que trazem sorrisos e um toque de diversão!',
-        imagem: AdesivoImg
-    },
-    {
-      id: 3,
-      nome: 'Caderno Inteligente',
-      preco: 'R$ 119,90',
-      descricao: 'Caderno vibrante que faz suas ideias brilharem!',
-      imagem: CadernoImg
-  }
-];
-
   return (
     <section className="App">
       <div className ="home">
